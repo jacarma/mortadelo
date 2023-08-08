70a03d215c6601032dbf82d5659863c7c3152f3f
jacarma <jacarma@gmail.com>
2023-08-09 00:01:26 +0200

    Update to gpt-3.5-turbo

--- index.mjs
+++ index.mjs
@@ -40,23 +40,20 @@ const tools = {
 
 // use GPT-3.5 to complete a given prompts
 const completePrompt = async (prompt) =>
-  await fetch("https://api.openai.com/v1/completions", {
+  await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + process.env.OPENAI_API_KEY,
     },
     body: JSON.stringify({
-      model: "text-davinci-003",
-      prompt,
-      max_tokens: 256,
-      temperature: 0.7,
-      stream: false,
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
       stop: ["Observación:"],
     }),
   })
     .then((res) => res.json())
-    .then((res) => res.choices[0].text)
+    .then((res) => res.choices[0].message.content)
     .then((res) => {
       // console.log("\x1b[90m" + prompt + "\x1b[0m");
       console.log("\x1b[90m" + res + "\x1b[0m");
@@ -86,7 +83,9 @@ const answerQuestion = async (question) => {
       const actionInput = response.match(
         /Entrada de la acción: "?(.*)"?/i
       )?.[1];
-      const result = await tools[action.trim()].execute(actionInput);
+      const result = tools[action.trim()]
+        ? await tools[action.trim()].execute(actionInput)
+        : "n/a";
       console.log(
         "\x1b[90m" + `${action}(${actionInput}): ${result}` + "\x1b[0m"
       );
